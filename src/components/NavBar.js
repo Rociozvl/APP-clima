862a23c2123912a4b1046c229878d4a10cb8b625
unknown <zavalaceleste98@gmail.com>
2023-02-06 14:46:17 -0300

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return(
+
+        <nav className="navbar text-light mb-5">
+            <div className="container-fluid">
+                <h3 className="mx-auto">Weather forecast</h3>
+            </div>
+        </nav>
+
+    );
+}
+
+export default NavBar;
